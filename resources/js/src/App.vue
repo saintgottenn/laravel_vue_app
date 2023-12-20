a251bc865ff572badffea05f3bbf1ba27ef9f817
saintgottenn <150365526+saintgottenn@users.noreply.github.com>
2023-12-20 18:35:14 +0300

    init

---
+++ resources/js/src/App.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div>{{ user }}</div>
+</template>
+<script>
+export default {
+    name: "App",
+
+    data() {
+        return {
+            user: "asdфывasd",
+        };
+    },
+};
+</script>
+<style lang=""></style>
